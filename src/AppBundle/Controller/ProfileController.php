b628190d81329e466dccd8795e1abb458a11350e
Andrey <folganoid@yahoo.com>
2017-08-20 20:58:35 +0300

    add edit profile fnc.

---
+++ src/AppBundle/Controller/ProfileController.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fg
+ * Date: 20.08.17
+ * Time: 16:21
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\ProfileType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * Class ProfileController
+ * @package AppBundle\Controller
+ */
+class ProfileController extends Controller
+{
+    /**
+     * @Route("/profile/{id}", name="profile")
+     */
+    public function profileAction($id, Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        if ($this->getUser()->getId() != $id) {
+            if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+                throw new \Exception('Access denied!');
+            }
+        }
+
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->findOneBy(
+            array('id' => $id)
+        );
+
+        if (!$user) {
+            throw $this->createNotFoundException('Unable to find User.');
+        }
+
+        $form = $this->createForm(ProfileType::class, null,
+            [
+                'username' => $user->getUsername(),
+                'email' => $user->getEmail(),
+                'role' => $user->getRole(),
+                'field' => ($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) ? true : false,
+            ]
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $usr = $em->getRepository(User::class)->find($id);
+
+            /**
+             * check exist user & email in DB
+             */
+            if ($data['username'] != $usr->getUsername())
+            {
+                if ($em->getRepository(User::class)->findByUsername($data['username']))
+                {
+                    throw new \Exception('User already exist!');
+                };
+            };
+
+            if ($data['email'] != $usr->getEmail())
+            {
+                if ($em->getRepository(User::class)->findByEmail($data['email']))
+                {
+                    throw new \Exception('Email already busy!');
+                };
+            };
+
+                $usr->setUsername($data['username']);
+                $usr->setEmail($data['email']);
+
+                if ($data['password']) {
+                    $usr->setPassword($encoder->encodePassword(new User(), $data['password']));
+                }
+
+            if ($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+                $usr->setRole($data['role']);
+            }
+
+                $em->persist($usr);
+                $em->flush();
+
+            $uri = $this->generateUrl('profile', array('id' => $id));
+            return $this->redirect($uri);
+
+        }
+
+        return $this->render('AppBundle:Profile:profile.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+}
--- src/AppBundle/Form/ProfileType.php
+++ src/AppBundle/Form/ProfileType.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace AppBundle\Form;
 
 use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -17,18 +20,32 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  * Date: 21.07.17
  * Time: 10:38
  */
-class UserType extends AbstractType
+class ProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('username', TextType::class)
-            ->add('email', EmailType::class)
+        $builder->add('username', TextType::class, array(
+            'attr' => [
+                'value' => $options['username'],
+            ]
+        ))
+            ->add('email', EmailType::class, array(
+                'attr' => [
+                    'value' => $options['email'],
+                ]
+            ))
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'The password fields must match.',
-                'first_options'  => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password'),
+                'first_options'  => array('label' => 'New password'),
+                'second_options' => array('label' => 'Repeat password'),
+                'required' => false,
             ])
+            ->add('role', ($options['field']) ? IntegerType::class : HiddenType::class, array(
+                'attr' => [
+                    'value' => $options['role'],
+                ],
+            ))
             ->add('submit', SubmitType::class, [
                 'attr' => [
                     'class' => 'uk-button uk-button-default uk-align-right"'
@@ -39,9 +56,10 @@ class UserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => User::class
+            'username' => null,
+            'email' => null,
+            'role' => null,
+            'field' => null,
         ]);
     }
 }
-
-
